829c5ce809ec81a2123ed61aa1b0ad691ec0cd15
Joshnaacsha <93771411+Joshnaacsha@users.noreply.github.com>
2025-03-16 16:59:49 +0530

    Delete server directory

--- server/socket.js
+++
@@ -1,8 +0,0 @@
-import WebSocketService from './services/WebSocketService.js';
-
-export const createSocketServer = (httpServer) => {
-    const webSocketService = new WebSocketService(httpServer);
-    return webSocketService.io;
-};
-
-export default createSocketServer; 
